c7b17c7cd889fd2b22217f2030f574df2a5306b4
dudu <test@gmail.com>
2026-02-16 16:20:33 -0700

    feat: Introduce a FastAPI application to provide SSE endpoints for remote redeployment and log viewing.

--- deployment/main.py
+++ deployment/main.py
@@ -67,4 +67,4 @@ async def view_logs(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="172.17.0.1", port=8000)
