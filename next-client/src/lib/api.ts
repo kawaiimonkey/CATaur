058c0f4dbcdac3f7d0d84f66ad0f5553af329cb9
dudu <test@gmail.com>
2026-02-17 13:20:20 -0700

    feat: Add Docker Compose configuration for Next.js client and update API base URL

--- next-client/src/lib/api.ts
+++ next-client/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:4000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3000";
 
 type RequestOptions = RequestInit & { skipAuth?: boolean };
 
